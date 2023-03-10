22ad3b8ea5452f7e14b480f92fc047a64502399b
Nikita Markelov <marknik139@Nikitas-MacBook-Pro.local>
2023-03-10 02:27:35 +0200

    fix mobile version missclick

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header () {
     return (
         <nav className="brown darken-1">
             <div className="nav-wrapper">
-                <a href="!#" className="brand-logo">Movies</a>
+                <span className="brand-logo left">Movies</span>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li><a href="https://github.com/marknik139/movies-omdb-api">Repo</a></li>
                 </ul>
